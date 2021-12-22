18e77c8b6088f0e27bd1a90e9e126047e4c826cf
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2021-12-22 15:01:06 +0300

    First version of models

--- ML_Bot/db/repositories/models/models.py
+++ ML_Bot/db/repositories/models/models.py
@@ -6,39 +6,48 @@ from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.dialects.postgresql import BYTEA
 from sqlalchemy.sql.sqltypes import Boolean, DateTime
 
-Base = declarative_base()
+from gino import Gino
 
-class User(Base):
-    __tablename__ = "user"
+db = Gino()
+#Base = declarative_base()
+
+class Message(db.Model):
+    __tablename__ = "message"
 
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    username = Column(String(128), nullable=True)
-    profession = Column(String(128))
-    programming_language = Column(String)
-    resumes = relationship("Resume", back_populates="user")
-    state = relationship("UserState", uselist=False, backref="user_state")
+    text = Column(String(256))
+    link_id = Column(Integer, ForeignKey("link.id"), nullable=False, index=True)
+    chat_id = Column(Integer)
+
+    # username = Column(String(128), nullable=True)
+    # profession = Column(String(128))
+    # programming_language = Column(String)
+    # resumes = relationship("Resume", back_populates="user")
+    # state = relationship("UserState", uselist=False, backref="user_state")
+
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime)
 
 
-class Resume(Base):
-    __tablename__ = "resume"
+class Link(db.Model):
+    __tablename__ = "link"
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
-    user = relationship("User", back_populates="resumes")
-    link = Column(String)
-    filename = Column(String)
-    extension = Column(String(8))
-    file_data = Column(BYTEA)
+    link = Column(String, index=True)
     created_at = Column(DateTime, nullable=False)
-    deleted_at = Column(DateTime)
 
-class UserState(Base):
-    __tablename__ = "user_state"
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False, index=True)
-    is_questioning = Column(Boolean)
+    # user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    # user = relationship("User", back_populates="resumes")
+    # link = Column(String)
+    # filename = Column(String)
+    # extension = Column(String(8))
+    # file_data = Column(BYTEA)
+
+
+# class UserState(db.Model):
+#     __tablename__ = "user_state"
+
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey("user.id"), nullable=False, index=True)
+#     is_questioning = Column(Boolean)
