60eeeafc2a87fbff180dbf68a131319d902223ac
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2021-12-24 11:09:18 +0300

    models with relationships

--- ML_Bot/db/repositories/models/models.py
+++ ML_Bot/db/repositories/models/models.py
@@ -13,7 +13,7 @@ class Message(Base):
 
     id = Column(Integer, primary_key=True)
     text = Column(String)
-
+    message_metadata = relationship("MessageMetadata", back_populates="message", uselist=False)
     # username = Column(String(128), nullable=True) msg msg link link
     # profession = Column(String(128))
     # programming_language = Column(String)
@@ -27,16 +27,19 @@ class MessageMetadata(Base):
     __tablename__ = "message_metadata"
 
     id = Column(Integer, primary_key=True)
-    link_id = Column(Integer, ForeignKey("link.id"), nullable=False, index=True)
     chat_id = Column(Integer)
     msg_id = Column(Integer, ForeignKey("message.id"), nullable=False, index=True)
+    message = relationship("Message", back_populates="message_metadata")
     user_id = Column(Integer)
+    links = relationship("Link", back_populates="message_metadata")
 
 
 class Link(Base):
     __tablename__ = "link"
 
     id = Column(Integer, primary_key=True)
+    message_metadata_id = Column(Integer, ForeignKey("message_metadata.id"), nullable=False)
+    message_metadata = relationship("MessageMetadata", back_populates="links")
     link = Column(String, index=True)
     created_at = Column(DateTime, nullable=False)
     deleted_at = Column(DateTime)
