c37f163ff3023b19ba26c7cbf3a613aa26697740
dmitrii-semenov <dmitrii.semenov@checkout.com>
2022-04-14 17:42:52 +0300

    Import config to db

--- ML_Bot/db/db_session_factory.py
+++ ML_Bot/db/db_session_factory.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import db_config
 
 engine = create_engine('postgresql+psycopg2://back:aHR!##9887ASDsda@bot_database:5432/ml_bot')
 # engine = create_engine(
